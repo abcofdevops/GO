cbcb7843056de4e13ed7e7e5a4fe3cb3a08122d3
ABCofDevops <76814002+abcofdevops@users.noreply.github.com>
2025-08-24 20:54:28 +0530

    fix: main.go

--- go-app/main.go
+++ go-app/main.go
@@ -5,17 +5,17 @@ import (
 	"net/http"
 )
 
-func server(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello from Go my server!")
+func home(w http.ResponseWriter, r *http.Request) {
+	// Render the home html page from static folder
+	http.ServeFile(w, r, "static/abcd.html")
 }
 
 func test(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello from Go Test Server!")
+    fmt.Fprintf(w, "Hello from Go Test!")
 }
 
 func main() {
-    http.HandleFunc("/", server)
+    http.HandleFunc("/", home)
     http.HandleFunc("/test", test)
     fmt.Println("Server running on port 8080")
     http.ListenAndServe(":8080", nil)
-}
